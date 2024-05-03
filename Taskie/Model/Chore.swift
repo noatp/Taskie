34965619fb412e6ccb2f91f0fc82c97641392d11
Toan Pham <toan.chpham@gmail.com>
2024-05-03 18:14:04 -0500

    Update Chore.swift

--- Taskie/Model/Chore.swift
+++ Taskie/Model/Chore.swift
@@ -8,14 +8,57 @@
 import Firebase
 
 struct Chore: Codable {
-    static let empty = Chore(id: "", name: "", creator: "", description: "", rewardAmount: 0.0, imageUrls: [], createdDate: .init(date: .now))
-    static let mock = Chore(id: "some id", name: "some name", creator: "some id", description: "some description", rewardAmount: 2.00, imageUrls: ["https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.istockphoto.com%2Fvector%2Fthumbnail-image-vector-graphic-gm1147544807-309589937&psig=AOvVaw3UvtbnqxN9ccX88jFYAgvQ&ust=1712642759035000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCOCb-8z5sYUDFQAAAAAdAAAAABAE"], createdDate: .init(date: .now))
+    static let empty = Chore(id: "", name: "", requestor: "", acceptor: nil, description: "", rewardAmount: 0.0, imageUrls: [], createdDate: .init(date: .now), finishedDate: nil)
+    static let mock = Chore(
+        id: "some id",
+        name: "This is a chore",
+        requestor: "some id",
+        acceptor: "some other id",
+        description: "This is a chore. It is a chore that need to be done. Please do it. You will be rewarded greatly. It is a chore that need to be done. Please do it. You will be rewarded greatly. It is a chore that need to be done. Please do it. You will be rewarded greatly.",
+        rewardAmount: 2.00,
+        imageUrls: ["https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.istockphoto.com%2Fvector%2Fthumbnail-image-vector-graphic-gm1147544807-309589937&psig=AOvVaw3UvtbnqxN9ccX88jFYAgvQ&ust=1712642759035000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCOCb-8z5sYUDFQAAAAAdAAAAABAE"],
+        createdDate: .init(date: .now),
+        finishedDate: .init(date: .now)
+    )
     
     let id: String
     let name: String
-    let creator: String
+    let requestor: String
+    let acceptor: String?
     let description: String
     let rewardAmount: Double
     let imageUrls: [String]
     let createdDate: Timestamp
+    let finishedDate: Timestamp?
+}
+
+struct ChoreForDetailView {
+    enum ActionButtonType {
+        case accept
+        case finish
+        case withdraw
+        case nothing
+    }
+    
+    static let empty: ChoreForDetailView = .init(
+        name: "",
+        requestorName: "",
+        acceptorName: "",
+        description: "",
+        rewardAmount: 0.0,
+        imageUrls: [],
+        createdDate: "",
+        finishedDate: nil,
+        actionButtonType: .accept
+    
+    )
+    let name: String
+    let requestorName: String
+    let acceptorName: String?
+    let description: String
+    let rewardAmount: Double
+    let imageUrls: [String]
+    let createdDate: String
+    let finishedDate: String?
+    let actionButtonType: ActionButtonType
 }
